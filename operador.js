0d03b96b134131946e3f91f1bc0bedd55a2ee184
sarilopsm20 <44045585+sarilopsm20@users.noreply.github.com>
2023-04-24 15:08:02 -0500

    main

--- operador.js
+++ operador.js
@@ -1,6 +1,8 @@
 //CONVERSIÓN LARGA:
-var celsius = prompt("Introduzca la temperatura en grados celsius: ");
-celsius = parseFloat(celsius);
+// var celsius = prompt("Introduzca la temperatura en grados celsius: ");
+// celsius = parseFloat(celsius);
 
 //CONVERSIÓN CORTA:
-
+var celsius = parseFloat(prompt("Introduzca la temperatura en grados celsius: "));
+var f = 9/5*celsius+32;
+document.write("La conversión es: ", f);
